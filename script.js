00f23fb50de6d2ff54390121d11813f9d2737747
YusufHayri <yusuf.hayri.erturk@gmail.com>
2021-03-08 14:52:30 +0300

    Ben gledim
    Bi bana da el atsaydınız fena olmazdı

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
-
 function legendfunction() {
     document.getElementById("legend").innerHTML = "Şaka şaka"
 }
 function newfnc(){
     window.open("https://github.com/omerfaruk749");
+
+}
+
+function youshuf(){
+    window.open("https://github.com/Yusuf-HE");
 
 }
