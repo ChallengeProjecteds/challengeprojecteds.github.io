52b8ba5dad9373385670420aebd2fe7dff26fde2
berkaydurur <brkydurur@gmail.com>
2021-04-10 14:03:57 +0300

    last changes

--- script.js
+++ script.js
@@ -13,5 +13,18 @@ function dmOnClick() {
     //body.style.backdrop-filter = bribrightness(0.5);
     //if(body.style.backdrop-filter)//bu özellikler çalışmıyor javascriptte
 }
-const button = document.querySelector('.DM');
+const button = document.querySelector('.dark-mode-button');
 button.addEventListener('click',dmOnClick);
+
+function toggleNav() {
+    if (document.getElementById("mySidebar").style.width == "250px") {
+        document.getElementById("baslik").style.left = "0px"
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    } 
+    else {
+        document.getElementById("baslik").style.left = "250px";
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+}
