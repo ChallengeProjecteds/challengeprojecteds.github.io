36b67e88cb30cec5d0b9a8279e0ed8af1412b8df
YusufHayri <yusuf.hayri@marun.edu.tr>
2021-04-11 15:48:01 +0300

    bootsrap card added

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ function dmOnClick() {
     //navbar-light bg-light
     //btn-ligt bg-light
     const button = document.querySelector('.dark-mode-button');
+    const nav = document.querySelector('nav');
     
     if(button.classList.contains('bg-light')) 
     {
         button.classList.remove('bg-light','btn-ligt');
         button.classList.add('bg-dark','btn-dark');
+        nav.classList.add('bg-light','navbar-ligt');
+        nav.classList.remove('bg-dark','navbar-dark');
     }
     else
     {
         button.classList.add('bg-light','btn-ligt');
         button.classList.remove('bg-dark','btn-dark');
+        nav.classList.remove('bg-light','navbar-ligt');
+        nav.classList.add('bg-dark','navbar-dark');
     }
     //body.style.backdrop-filter = bribrightness(0.5);
     //if(body.style.backdrop-filter)//bu özellikler çalışmıyor javascriptte
