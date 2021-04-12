16550dac0fa80b0941a4dd23f99fb4fcfe65b3dd
berkaydurur <brkydurur@gmail.com>
2021-04-12 12:19:10 +0300

    last final version

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function dmOnClick() {
     const button = document.querySelector('.dark-mode-button');
     const nav = document.querySelector('nav');
+    const header = document.querySelector('header');
     
     if(button.classList.contains('bg-light')) 
     {
@@ -8,6 +9,8 @@ function dmOnClick() {
         button.classList.add('bg-dark','btn-dark');
         nav.classList.add('bg-light','navbar-ligt');
         nav.classList.remove('bg-dark','navbar-dark');
+        header.classList.add('bg-light','navbar-ligt');
+        header.classList.remove('bg-dark','navbar-dark');
     }
     else
     {
@@ -15,8 +18,12 @@ function dmOnClick() {
         button.classList.remove('bg-dark','btn-dark');
         nav.classList.remove('bg-light','navbar-ligt');
         nav.classList.add('bg-dark','navbar-dark');
+        header.classList.remove('bg-light','navbar-ligt');
+        header.classList.add('bg-dark','navbar-dark');
+        
         
     }
 }
 const button = document.querySelector('.dark-mode-button');
 button.addEventListener('click',dmOnClick);
+
